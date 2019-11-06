b167c6283e268b1d17d8af3d2d9c39015c546eb4
Erik Jansen <erik.gunnar.jansen@nav.no>
2019-11-06 08:44:01 +0100

    Remove ledetekster from AvbruttMote

--- js/components/moter/moteplanlegger/AvbruttMote.js
+++ js/components/moter/moteplanlegger/AvbruttMote.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import {
-    getLedetekst,
-    getHtmlLedetekst,
-} from '@navikt/digisyfo-npm';
 import { motePt } from '../../../propTypes';
 import { visDato, visKlokkeslett } from '../../../utils/datoUtils';
 
+/* eslint-disable max-len */
+const texts = {
+    title: 'Status',
+    subTitle: 'Møte avbrutt',
+    blokkIngress: 'Møteforespørsel og tidspunkter det gjelder:',
+    tid: 'kl.',
+    sted: 'Møtested:',
+    sporsmal: 'Har du spørsmål, kan du kontakte oss på 55 55 33 33',
+    forklarende: {
+        bekreftet: 'Du har tidligere mottatt en bekreftelse på et tidspunkt for et dialogmøte med NAV og din arbeidsgiver. Møteforespørselen er avbrutt, og du kan se bort fra denne forespørselen. Er det fortsatt aktuelt med et møte, vil du få en ny forespørsel.',
+        avbrutt: 'Du har tidligere mottatt en møteforespørsel med tidspunkter for et dialogmøte med NAV og din arbeidsgiver. Møteforespørselen er avbrutt, og du kan se bort fra denne forespørselen. Er det fortsatt aktuelt med et møte, vil du få en ny forespørsel.',
+    },
+};
+/* eslint-enable max-len */
+
 export const forklarendeTekst = (mote) => {
     return mote.bekreftetTidspunkt
-        ? getHtmlLedetekst('mote.avbruttmote.sykmeldt.bekreftet_sa_avbrutt')
-        : getHtmlLedetekst('mote.avbruttmote.sykmeldt.avbrutt');
+        ? texts.forklarende.bekreftet
+        : texts.forklarende.avbrutt;
 };
 
 const AvbruttMote = (
@@ -17,36 +28,36 @@ const AvbruttMote = (
         mote,
     },
 ) => {
-    const deltakertypeSuffix = 'arbeidstaker';
-    const harDuSporsmalNokkel = `mote.avbruttmote.har_du_sporsmal.${deltakertypeSuffix}`;
     return (
         <div>
             <header className="sidetopp">
-                <h1 className="sidetopp__tittel">{getLedetekst('mote.avbruttmote.tittel')}</h1>
+                <h1 className="sidetopp__tittel">{texts.title}</h1>
             </header>
             <div className="panel">
                 <div className="illustrertTittel">
                     <img className="illustrertTittel__img" src={`${process.env.REACT_APP_CONTEXT_ROOT}/img/svg/mote_avbrutt.svg`} alt="" />
-                    <h2 className="illustrertTittel__tittel">{getLedetekst('mote.avbruttmote.undertittel')}</h2>
+                    <h2 className="illustrertTittel__tittel">{texts.subTitle}</h2>
                 </div>
-                <div dangerouslySetInnerHTML={forklarendeTekst(mote)} />
+                <div>
+                    <p>{forklarendeTekst(mote)}</p>
+                </div>
                 <div className="adskilt__blokk blokk">
-                    <h3 className="typo-element">{getLedetekst('mote.avbruttmote.tidspunkter_det_gjelder')}</h3>
+                    <h3 className="typo-element">{texts.blokkIngress}</h3>
                     <div className="kvittering__svar blokk">
                         { mote.alternativer.map((alternativ) => {
                             return (
                                 // eslint-disable-next-line jsx-a11y/label-has-associated-control
                                 <label key={alternativ.id} className="avbrutt__mote__svar">
-                                    {`${visDato(alternativ.tid)} ${getLedetekst('mote.kvittering.kl')} ${visKlokkeslett(alternativ.tid)}`}
+                                    {`${visDato(alternativ.tid)} ${texts.tid} ${visKlokkeslett(alternativ.tid)}`}
                                 </label>
                             );
                         })
                         }
                     </div>
-                    <h3 className="typo-element">{getLedetekst('mote.avbruttmote.motested')}</h3>
+                    <h3 className="typo-element">{texts.sted}</h3>
                     <p>{mote.alternativer[0].sted}</p>
                 </div>
-                <p>{getLedetekst(harDuSporsmalNokkel)}</p>
+                <p>{texts.sporsmal}</p>
             </div>
         </div>
     );
