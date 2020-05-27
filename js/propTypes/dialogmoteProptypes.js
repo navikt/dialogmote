526ac6a00bd2d049b39d87807e47307361fcde2e
Erik Jansen <erik.gunnar.jansen@nav.no>
2020-05-27 12:51:45 +0200

    Fix proptype for motebehovStatus

--- js/propTypes/dialogmoteProptypes.js
+++ js/propTypes/dialogmoteProptypes.js
@@ -11,11 +11,17 @@ export const motebehovPt = PropTypes.shape({
     }),
 });
 
+export const motebehovStatusPt = PropTypes.shape({
+    visMotebehov: PropTypes.bool,
+    skjemaType: PropTypes.string,
+    motebehovSvar: motebehovPt,
+});
+
 export const motebehovReducerPt = PropTypes.shape({
     henter: PropTypes.bool,
     hentet: PropTypes.bool,
     hentingFeilet: PropTypes.bool,
-    data: PropTypes.arrayOf(motebehovPt),
+    data: motebehovStatusPt,
 });
 
 export const motebehovSvarReducerPt = PropTypes.shape({
