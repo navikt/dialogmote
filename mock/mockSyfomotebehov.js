087665304af269fe79461684536d6691301f7527
Erik Jansen <erik.gunnar.jansen@nav.no>
2020-05-27 12:27:19 +0200

    Refactor mockSyfomotebehov

--- mock/mockSyfomotebehov.js
+++ mock/mockSyfomotebehov.js
@@ -1,20 +1,52 @@
-const motebehovStatus = {
-    visMotebehov: true,
-    skjemaType: 'SVAR_BEHOV',
-    motebehov: {
-        arbeidstakerFnr: '02020212345',
-        opprettetAv: '',
-        virksomhetsnummer: '000111222',
-        motebehovSvar: {
-            harMotebehov: true,
-        },
+const motebehovSvar = {
+    arbeidstakerFnr: '02020212345',
+    opprettetAv: '',
+    virksomhetsnummer: '000111222',
+    motebehovSvar: {
+        harMotebehov: true,
     },
 };
+
+const motebehovStatusSvarUnavailable = {
+    visMotebehov: true,
+    skjemaType: 'SVAR_BEHOV',
+    motebehov: null,
+};
+
+const motebehovStatusSvarBehov = {
+    visMotebehov: true,
+    skjemaType: 'SVAR_BEHOV',
+    motebehov: null,
+};
+const motebehovStatusSvarBehovSvar = {
+    visMotebehov: true,
+    skjemaType: 'SVAR_BEHOV',
+    motebehov: motebehovSvar,
+};
+
+const motebehovStatusEnum = {
+    SVAR_BEHOV: 'SVAR_BEHOV',
+    SVAR_BEHOV_SVAR: 'SVAR_BEHOV_SVAR',
+};
+
+function getMotebehovStatus(type) {
+    switch (type) {
+        case motebehovStatusEnum.SVAR_BEHOV: {
+            return motebehovStatusSvarBehov;
+        }
+        case motebehovStatusEnum.SVAR_BEHOV_SVAR: {
+            return motebehovStatusSvarBehovSvar;
+        }
+        default: {
+            return motebehovStatusSvarUnavailable;
+        }
+    }
+}
 
 function mockPilotEndepunkterForLokalmiljo(server) {
     server.get('/syfomotebehov/api/v2/arbeidstaker/motebehov', (req, res) => {
         res.setHeader('Content-Type', 'application/json');
-        res.send(JSON.stringify(motebehovStatus));
+        res.send(JSON.stringify(getMotebehovStatus(motebehovStatusEnum.SVAR_BEHOV)));
     });
 
     server.post('/syfomotebehov/api/v2/arbeidstaker/motebehov', (req, res) => {
