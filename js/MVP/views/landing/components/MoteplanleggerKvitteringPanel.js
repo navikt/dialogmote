00899103d08026440a20a8ce24cf64d3e240c709
Tanja Krasnomovets <tanja.krasnomovets@nav.no>
2021-08-27 15:41:28 +0200

    Forenklet kvitteringer panel

--- js/MVP/views/landing/components/MoteplanleggerKvitteringPanel.js
+++ js/MVP/views/landing/components/MoteplanleggerKvitteringPanel.js
@@ -1,18 +1,14 @@
-import { Hovedknapp } from 'nav-frontend-knapper';
+import { Knapp } from 'nav-frontend-knapper';
 import ModalWrapper from 'nav-frontend-modal';
+import Tekstomrade from 'nav-frontend-tekstomrade';
 import PropTypes from 'prop-types';
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import BekreftelseKvittering from '../../moteplanlegger/components/BekreftelseKvittering';
-import { BRUKER } from '../../../../enums/moteplanleggerDeltakerTyper';
 import { motePt } from '../../../../propTypes';
-import { BEKREFTET, getSvarsideModus, MOTESTATUS } from '../../../../utils/moteUtils';
+import { BEKREFTET, MOTESTATUS } from '../../../../utils/moteUtils';
 import DialogmotePanel from '../../../containers/DialogmotePanel';
+import BekreftelseKvittering from '../../moteplanlegger/components/BekreftelseKvittering';
 import SvarKvittering from '../../moteplanlegger/components/SvarKvittering';
-
-const SectionStyled = styled.section`
-  margin: 32px 0;
-`;
 
 const texts = {
   titleSvart: 'Du har svart på tidspunkt for dialogmøte',
@@ -25,94 +21,59 @@ const texts = {
   buttonBekreftet: 'SE BEKREFTELSEN',
 };
 
-const getMotePlanleggingsKvitteringInnhold = (modus, mote) => {
-  if (modus === BEKREFTET) {
-    return <BekreftelseKvittering mote={mote} />;
-  }
-  if (modus === MOTESTATUS) {
-    return <SvarKvittering mote={mote} />;
-  }
-  return null;
-};
+const DialogmotePanelStyled = styled(DialogmotePanel)`
+  margin-bottom: 32px;
+`;
 
-const Modal = ({ isOpen, setIsOpen, content }) => {
-  return (
-    <ModalWrapper
-      isOpen={isOpen}
-      onRequestClose={() => setIsOpen(false)}
-      closeButton
-      contentLabel="Møteplanlegger kvittering"
-    >
-      <div style={{ padding: '2rem 2.5rem', width: '31rem' }}>{content}</div>
-    </ModalWrapper>
-  );
-};
-
-Modal.propTypes = {
-  isOpen: PropTypes.bool,
-  setIsOpen: PropTypes.func,
-  content: PropTypes.object,
-};
-
-const Panel = ({ title, text, icon, buttonText, setIsModalOpen, isModalOpen, mote }) => {
-  const modus = getSvarsideModus(mote, BRUKER);
-
-  return (
-    <DialogmotePanel title={title} icon={icon}>
-      <SectionStyled>{text}</SectionStyled>
-      <Hovedknapp onClick={() => setIsModalOpen(true)} mini>
-        {buttonText}
-      </Hovedknapp>
-
-      <Modal
-        setIsOpen={setIsModalOpen}
-        isOpen={isModalOpen}
-        content={getMotePlanleggingsKvitteringInnhold(modus, mote)}
-      />
-    </DialogmotePanel>
-  );
-};
-
-Panel.propTypes = {
-  title: PropTypes.string,
-  text: PropTypes.string,
-  icon: PropTypes.string,
-  buttonText: PropTypes.string,
-  setIsModalOpen: PropTypes.func,
-  isModalOpen: PropTypes.bool,
-  mote: motePt,
-};
+const TekstomradeStyled = styled(Tekstomrade)`
+  margin: 32px 0;
+`;
 
 const MoteplanleggerKvitteringPanel = ({ mote, modus }) => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
   if (modus === BEKREFTET) {
     return (
-      <Panel
-        title={texts.titleBekreftet}
-        text={texts.titleBekreftet}
-        icon="kalender-innkalling"
-        buttonText={texts.buttonBekreftet}
-        setIsModalOpen={setIsOpen}
-        isModalOpen={isOpen}
-        mote={mote}
-      />
+      <DialogmotePanelStyled title={texts.titleBekreftet} icon="kalender-innkalling">
+        <TekstomradeStyled>{texts.textBekreftet}</TekstomradeStyled>
+        <ModalWrapper
+          isOpen={isModalOpen}
+          onRequestClose={() => setIsModalOpen(false)}
+          closeButton
+          contentLabel="Møteplanlegger kvittering"
+        >
+          <div style={{ padding: '2rem 2.5rem', width: '31rem' }}>
+            <BekreftelseKvittering mote={mote} />
+          </div>
+        </ModalWrapper>
+
+        <Knapp mini onClick={() => setIsModalOpen(true)}>
+          {texts.buttonBekreftet}
+        </Knapp>
+      </DialogmotePanelStyled>
     );
   }
-
   if (modus === MOTESTATUS) {
     return (
-      <Panel
-        title={texts.titleSvart}
-        text={texts.textSvart}
-        icon="kalender-innkalling"
-        buttonText={texts.buttonSvart}
-        setIsModalOpen={setIsOpen}
-        isModalOpen={isOpen}
-        mote={mote}
-      />
+      <DialogmotePanelStyled title={texts.titleSvart} icon="kalender-innkalling">
+        <TekstomradeStyled>{texts.textSvart}</TekstomradeStyled>
+        <ModalWrapper
+          isOpen={isModalOpen}
+          onRequestClose={() => setIsModalOpen(false)}
+          closeButton
+          contentLabel="Møteplanlegger kvittering"
+        >
+          <div style={{ padding: '2rem 2.5rem', width: '31rem' }}>
+            <SvarKvittering mote={mote} />
+          </div>
+        </ModalWrapper>
+
+        <Knapp mini onClick={() => setIsModalOpen(true)}>
+          {texts.buttonSvart}
+        </Knapp>
+      </DialogmotePanelStyled>
     );
   }
-  return null;
+  return <div />;
 };
 
 MoteplanleggerKvitteringPanel.propTypes = { mote: motePt, modus: PropTypes.string };
