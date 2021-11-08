5990c64d0962d56189fabff8a91cf23e8c6cf8d2
Luan Tran <8613937+luantr@users.noreply.github.com>
2021-11-08 09:50:01 +0100

    fix render logic of avbrutt moteplanlegger (#91)

--- js/utils/moteUtils.js
+++ js/utils/moteUtils.js
@@ -27,7 +27,7 @@ export const erMotePassert = (mote) => {
   const antallAlternativer = mote.alternativer.length;
   return (
     mote.alternativer.filter((alternativ) => {
-      return alternativ.tid <= newDate();
+      return new Date(alternativ.tid) <= newDate();
     }).length === antallAlternativer
   );
 };
