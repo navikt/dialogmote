1db8254f8a0a14e4318a7c56db10882a368be11e
Kaja Kvinge Akselsen <kaja.kvinge.akselsen@nav.no>
2021-12-07 12:36:24 +0100

    uRqbZCAi: Prettier

--- js/MVP/views/moteinnkallelse/components/DittSvarPaInnkallelse.tsx
+++ js/MVP/views/moteinnkallelse/components/DittSvarPaInnkallelse.tsx
@@ -1,4 +1,4 @@
-import React, {ReactElement} from 'react';
+import React, { ReactElement } from 'react';
 import { AlertStripeSuksess } from 'nav-frontend-alertstriper';
 import { svarInnkallelseTypes } from '@/MVP/globals/constants';
 import Tekstomrade from 'nav-frontend-tekstomrade';
