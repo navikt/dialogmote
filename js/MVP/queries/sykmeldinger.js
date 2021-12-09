74f2aa9caf217de7dc26ae550d30c55e77e29a50
Kaja Kvinge Akselsen <kaja.kvinge.akselsen@nav.no>
2021-12-09 13:59:10 +0100

    Type response from backend for sykmeldinger

--- js/MVP/queries/sykmeldinger.js
+++
@@ -1,8 +0,0 @@
-import { useQuery } from 'react-query';
-import { getSyfoopfolgingsplanservice } from '../services/sykmeldinger';
-
-const SYKMELDINGER = 'sykmeldinger';
-
-export const useSykmeldinger = () => {
-  return useQuery(SYKMELDINGER, getSyfoopfolgingsplanservice);
-};
