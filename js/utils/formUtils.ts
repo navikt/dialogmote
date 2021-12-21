7b1605dac8410ad4d7303273cf978d21b3a9f778
Kaja Kvinge Akselsen <kaja.kvinge.akselsen@nav.no>
2021-12-21 10:38:30 +0100

    Use react-hook-form for begrunnelse

--- js/utils/formUtils.ts
+++ js/utils/formUtils.ts
@@ -1,17 +1,17 @@
-import {FieldErrors} from "react-hook-form";
-import {FeiloppsummeringFeil} from "nav-frontend-skjema";
+import { FieldErrors } from 'react-hook-form';
+import { FeiloppsummeringFeil } from 'nav-frontend-skjema';
 
 export const mapErrors = (errors: FieldErrors): FeiloppsummeringFeil[] => {
-    const feil: FeiloppsummeringFeil[] = [];
-    const keys = Object.keys(errors)
-    keys.forEach((key) => {
-        if (errors[key]) {
-            feil.push({
-                skjemaelementId: key,
-                feilmelding: errors[key].message,
-            });
-        }
-    });
+  const feil: FeiloppsummeringFeil[] = [];
+  const keys = Object.keys(errors);
+  keys.forEach((key) => {
+    if (errors[key]) {
+      feil.push({
+        skjemaelementId: key,
+        feilmelding: errors[key].message,
+      });
+    }
+  });
 
-    return feil;
+  return feil;
 };
