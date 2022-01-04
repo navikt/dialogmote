d163901669eb6d0bdc9c2f23831ae4ea110844f2
Kaja Kvinge Akselsen <kaja.kvinge.akselsen@nav.no>
2022-01-04 15:51:17 +0100

    Debug logging

--- server/appProxy.js
+++ server/appProxy.js
@@ -21,12 +21,20 @@ const appProxy = (server) => {
       },
       onProxyReq(proxyReq, req, res) {
         console.log('I onProxyReq');
+          var body = '';
+          req.on('data', function (data) {
+              data = data.toString();
+              body += data;
+              console.log(body);
+          });
         const token = req.cookies['selvbetjening-idtoken'];
         proxyReq.setHeader('Content-Type', 'application/json');
         proxyReq.setHeader('Authorization', `Bearer ${token}`);
       },
       onProxyRes(proxyRes, req, res) {
         console.log('I onProxyRes');
+        console.log('Statuskode:')
+          console.log(res.statusCode)
         var body = '';
         proxyRes.on('data', function (data) {
           data = data.toString();
