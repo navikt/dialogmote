f8d57306104a21f742003e49012bc26378e4d6b0
Luan Tran <8613937+luantr@users.noreply.github.com>
2022-01-04 16:10:38 +0100

    wip

--- server/appProxy.js
+++ server/appProxy.js
@@ -20,27 +20,15 @@ const appProxy = (server) => {
         res.end();
       },
       onProxyReq(proxyReq, req, res) {
-        console.log('I onProxyReq');
-          var body = '';
-          req.on('data', function (data) {
-              data = data.toString();
-              body += data;
-              console.log(body);
-          });
         const token = req.cookies['selvbetjening-idtoken'];
         proxyReq.setHeader('Content-Type', 'application/json');
         proxyReq.setHeader('Authorization', `Bearer ${token}`);
-      },
-      onProxyRes(proxyRes, req, res) {
-        console.log('I onProxyRes');
-        console.log('Statuskode:')
-          console.log(res.statusCode)
-        var body = '';
-        proxyRes.on('data', function (data) {
-          data = data.toString();
-          body += data;
-          console.log(body);
-        });
+
+        if (req.body) {
+          const bodyData = JSON.stringify(req.body);
+          proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
+          proxyReq.write(bodyData);
+        }
       },
       logLevel: 'debug',
       changeOrigin: true,
