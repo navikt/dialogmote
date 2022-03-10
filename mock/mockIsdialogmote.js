ef98ad7919c521460e34321d923e77041063af28
Luan Tran <8613937+luantr@users.noreply.github.com>
2022-03-10 22:23:33 +0100

    Update labs mocks

--- mock/mockIsdialogmote.js
+++ mock/mockIsdialogmote.js
@@ -1,9 +1,11 @@
 const referat = require('../mock/data/MVP/referatByteArray.json');
+const innkallelse = require('./data/MVP/innkallelse2');
+const referatBrev = require('./data/MVP/referat');
 
 function mockSyfomoteadmin(server) {
   server.get('/syk/dialogmote/api/v1/arbeidstaker/brev', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify([]));
+    res.send(JSON.stringify([innkallelse, referatBrev]));
     // res.status(400);
     // res.send('Bad request');
   });
