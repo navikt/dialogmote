c90bc44eb4c91bb85a020132c7768b7f948ce8f0
Luan Tran <8613937+luantr@users.noreply.github.com>
2022-03-11 09:05:41 +0100

    Update labs mocks (#133)

--- mock/mockSyfomoteadmin.js
+++ mock/mockSyfomoteadmin.js
@@ -1,19 +1,12 @@
 const mockData = require('./mockData');
 const enums = require('./mockDataEnums');
 
-function mockSyfomoteadmin(server, erLokal) {
-  if (erLokal) {
-    server.get('/syfomoteadmin/api/bruker/arbeidstaker/moter/siste', (req, res) => {
-      res.setHeader('Content-Type', 'application/json');
-      res.send(JSON.stringify(mockData[enums.SISTE]));
-      // res.status(404);
-      // res.send('NotFound');
-    });
-  }
-
+function mockSyfomoteadmin(server) {
   server.get('/syfomoteadmin/api/bruker/arbeidstaker/moter/siste', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(mockData[enums.SISTE]));
+    //res.send(JSON.stringify(mockData[enums.SISTE]));
+    res.status(404);
+    res.send('NotFound');
   });
 }
 
